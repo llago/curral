c86dcb3d91b5b79a216576b994cfd64420e8b85f
lucas <llagocarvalho@gmail.com>
2015-08-05 00:57:16 -0300

    outros terminais

--- riobus-analytics/public/js/relatorio/curral.js
+++ riobus-analytics/public/js/relatorio/curral.js
@@ -1,12 +1,31 @@
 var coordinate = new google.maps.LatLng(-22.907073, -43.320531);                                                                                                                                                                                                       
 
-var retanguloTesteCoords = [
+var polyAlvorada = [
 	new google.maps.LatLng(-23.00084,-43.3675),
 	new google.maps.LatLng(-23.00074,-43.3648),
 	new google.maps.LatLng(-23.00173,-43.36461),
 	new google.maps.LatLng(-23.00184,-43.36618),
 	new google.maps.LatLng(-23.00187,-43.36746)
-  ];
+	];
+
+var polyCampoGrande = [
+	new google.maps.LatLng(-22.90167,-43.55616),
+	new google.maps.LatLng(-22.90211,-43.55612),
+	new google.maps.LatLng(-22.90234,-43.55579),
+	new google.maps.LatLng(-22.90205,-43.55421),
+	new google.maps.LatLng(-22.9013,-43.55416),
+	new google.maps.LatLng(-22.90143,-43.55521)
+	];  
+var polyProcopio = [
+	new google.maps.LatLng(-22.90482,-43.19305),
+	new google.maps.LatLng(-22.90509,-43.19313),
+	new google.maps.LatLng(-22.90527,-43.19299),
+	new google.maps.LatLng(-22.90489,-43.19175),
+	new google.maps.LatLng(-22.90454,-43.19185),
+	new google.maps.LatLng(-22.90464,-43.19247)
+	];
+
+var rodoviaria;
 
 var analysesData = function(data) {
 		var newData = [];
@@ -18,19 +37,47 @@ var analysesData = function(data) {
 		return newData;
 	}
 
-var	poligonoteste = new google.maps.Polygon({
-	    paths: retanguloTesteCoords,
-	    strokeColor: '#FF0000',
-	    strokeOpacity: 0.8,
-	    strokeWeight: 3,
-	    fillColor: '#FF0000',
-	    fillOpacity: 0.35
-  	});
+function createPolygon(rodoviaria){
+
+	var	finalPolygon = new google.maps.Polygon({
+		    paths: rodoviaria,
+		    strokeColor: '#FF0000',
+		    strokeOpacity: 0.8,
+		    strokeWeight: 3,
+		    fillColor: '#FF0000',
+		    fillOpacity: 0.35
+	  	});
+	return finalPolygon;
+}
 
 var isWithinPolygon = poligonoteste.containsLatLng(coordinate);
 
 
 
+function createHtml (onibus) {
+
+	var title = '' + onibus[1];
+
+    if(onibus[2]){
+    	var desc = ' <div class="col-sm-3"> '+ onibus[2] + ' </div>';
+    	}
+    
+    else{
+         var desc = ' <div class="col-sm-3"> ônibus sem linha </div>';
+        }
+
+    desc += ' <div class="col-sm-3"> '+  onibus[0] + '  </div>';
+            
+    var response = '<div class="col-sm-12">';
+    response += '<h4 class="col-sm-3"><span class="col-sm-6">'+title+'</span>';
+            
+	response += '</h4>';
+	response += ''+desc + '</div>';
+
+	response += "</div>";
+    document.getElementById("resposta").innerHTML += response;
+  		
+}
 
 
 
@@ -67,7 +114,9 @@ function runSearch() {
     //finalDate = moment(finalDate,"DD/MM/YYYY HH:mm:ss").format("YYYY-MM-DD HH:mm:ss");
 	initialDate = moment(initialDate,"DD/MM/YYYY - HH").format("YYYYMMDDHH");
     finalDate = moment(finalDate,"DD/MM/YYYY - HH").format("YYYYMMDDHH");
-    url = "http://rest.riob.us:81/api/" + initialDate + "/" + finalDate + "?callback=?"
+    url = "http://rest.riob.us:81/api/" + initialDate + "/" + finalDate + "?callback=?";
+
+
 
 	$.getJSON(url, function(data, status){
 		data = [data];
@@ -75,48 +124,61 @@ function runSearch() {
 		var contador = 0;
 		var onibusdentro = [];
 		//var linha = data[4].ORDEM;
+
+		var rodoviaria;
+
+		if (document.getElementById('alvorada').checked) {
+  		rodoviaria =window [document.getElementById('alvorada').value];
+		}
+
+		if (document.getElementById('campoGrande').checked) {
+  		rodoviaria = window [document.getElementById('campoGrande').value];
+		}
+
+		if (document.getElementById('procopio').checked) {
+  		rodoviaria = window [document.getElementById('procopio').value];
+		}
+
+		var finalPolygon=createPolygon(rodoviaria);
+
 		for(var j=0;j<data[0].length;j++){
 			for(var i=0;i<data[0][j].DATA.length;i++){
 				var lat = data[0][j].DATA[i][3];
 				var longi = data[0][j].DATA[i][4];
 				var coordinate = new google.maps.LatLng(lat,longi); 
-				var isWithinPolygon = poligonoteste.containsLatLng(coordinate);
+				var isWithinPolygon = finalPolygon.containsLatLng(coordinate);
 
 				if(isWithinPolygon){
-					//console.log(+lat+","+longi);
+					console.log(+lat+","+longi);
 					onibusdentro[contador] = data[0][j].DATA[i];
 					contador++;
+					
 				}
 			}
 		}
-	
-	
-		for(var z=0;z<onibusdentro.length;z++){
-			
-			onibus = onibusdentro[z];
-			var title = '' + onibus[1];
-            if(onibus[2]){
-            var desc = ' <div class="col-sm-3"> '+ onibus[2] + ' </div>';
-            }
-            else{
-            	var desc = ' <div class="col-sm-3"> ônibus sem linha </div>';
-            }
 
-            desc += ' <div class="col-sm-3"> '+  onibus[0] + '  </div>';
-            
-            var response = '<div class="col-sm-12">';
-            response += '<h4 class="col-sm-3"><span class="col-sm-6">'+title+'</span>';
-            
-            response += '</h4>';
-            response += ''+desc + '</div>';
-
-            response += "</div>";
-            document.getElementById("resposta").innerHTML += response;
-  		
+		for (var val of onibusdentro) {
+    		createHtml(val);
+    		console.log()
 		}
+		// onibusdentro.forEach(createHtml(entry));
+  //   		//createHtml(entry);
+  //   	}
+
+		//$.each(onibusdentro, function(i ,item){
+
+        //	createHtml(item);
+        //}
+
+		// for(var z=0;z<onibusdentro.length;z++){
+			
+		// 	onibus = onibusdentro[z];
+		// 	createHtml(onibus);
+
+		// }
 		console.log("Numero de onibus:"+contador);
 		endLoadingAnimation();
 	});
        
- }
+}
 
